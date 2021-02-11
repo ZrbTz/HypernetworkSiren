c5a193f54f1a5765fe74344a731d743b1a3fb882
simo-cava <66009458+simo-cava@users.noreply.github.com>
2021-02-11 08:51:32 +0100

    Update other_hypernetworks.py
    add VGG

--- hypersiren/other_hypernetworks.py
+++ hypersiren/other_hypernetworks.py
@@ -99,4 +99,104 @@ class InceptionAndResidual(nn.Module):
         return model
 
 
-####################################################################################
+########################################################################
+# VGG Convolutional Network
+model_urls = {
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+}
+
+class VGG(nn.Module):
+
+    def __init__(self, features, layerSizes, init_weights=True):
+        super(VGG, self).__init__()
+
+        self.layerSizes = layerSizes
+        '''self.totalNumberOfWeights = sum(x[0] * x[1] for x in self.layerSizes)
+        self.totalNumberOfBiases = sum(x[1] for x in self.layerSizes)'''
+
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        
+        self.generateWeights = nn.ModuleList()
+
+        for layer in self.layerSizes:
+            self.generateWeights.append(nn.Linear(512 * 7 * 7, (layer[0] + 1 ) * layer[1]))
+            
+        '''self.generateWeights = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, self.totalNumberOfWeights + self.totalNumberOfBiases),
+        )'''
+    
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        weights = []
+        for generateW in self.generateWeights:
+            weights.append(generateW(x))
+
+        weight_outputs = torch.cat(weights, dim=1)
+
+        '''weight_outputs = self.generateWeights(x)'''
+ 
+        return weight_outputs, None, None
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfgs = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def _vgg(arch, cfg,  layerSizes, batch_norm, pretrained, progress, **kwargs):
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm),  layerSizes, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+def vgg19(layerSizes, pretrained=False, progress=True, **kwargs):
+    return _vgg('vgg19', 'E',  layerSizes, False, pretrained, progress, **kwargs)
